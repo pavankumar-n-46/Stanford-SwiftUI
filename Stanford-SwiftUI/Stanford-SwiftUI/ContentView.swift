93301fb385882d4d5b2dd1ef21434863d15d4dc9
Pavan Kumar <pavan11.kumar@paytmmoney.com>
2020-05-24 12:42:49 +0530

    Project Initial commit.

---
+++ Stanford-SwiftUI/Stanford-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stanford-SwiftUI
+//
+//  Created by Pavan Kumar on 24/05/20.
+//  Copyright © 2020 https://github.com/pavankumar-n-46/Stanford-SwiftUI . All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
